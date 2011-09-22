f5bda433dd3157eabc26d3cf0870f60779cc8c1c
Scott Little <scott@littleknownsoftware.com>
2011-09-22 16:44:57 +0200

    Update animated background image.

--- Mail Bundle Manager/Mail Bundle Manager/MBMAnimatedListController.m
+++ Mail Bundle Manager/Mail Bundle Manager/MBMAnimatedListController.m
@@ -10,16 +10,23 @@
 
 #import "LKCGStructs.h"
 
-#define TEXT_FIELD_X			20.0f
+#define TEXT_FIELD_X			10.0f
 #define TEXT_FIELD_WIDTH		150.0f
 #define TEXT_FIELD_HEIGHT		23.0f
 #define TEXT_FIELD_TOP_OFFSET	25.0f
 #define TEXT_FIELD_OFFSET		10.0f
-#define ANIMATOR_VIEW_X			2.0f
+#define ANIMATOR_VIEW_X			0.0f
 #define ANIMATOR_TOP_OFFSET		19.0f
-#define ANIMATOR_VIEW_WIDTH		200.0f
+#define ANIMATOR_VIEW_WIDTH		210.0f
 #define ANIMATOR_VIEW_HEIGHT	30.0f
 
+#define TEXT_UNSELECTED_COLOR	[NSColor colorWithDeviceWhite:0.323 alpha:1.000]
+#define TEXT_SELECTED_COLOR		[NSColor colorWithDeviceWhite:0.945 alpha:1.000]
+
+
+@interface MBMAnimatedListController ()
+- (void)animateMarkerFromItem:(NSInteger)fromItem toItem:(NSInteger)toItem;
+@end
 
 @implementation MBMAnimatedListController
 
@@ -38,6 +45,7 @@
 		//	Create the animator view for the moment that shows the step
 		NSImageView	*animatorBGView = [[NSImageView alloc] initWithFrame:NSMakeRect(ANIMATOR_VIEW_X, aView.frame.size.height - (ANIMATOR_TOP_OFFSET + ANIMATOR_VIEW_HEIGHT), ANIMATOR_VIEW_WIDTH, ANIMATOR_VIEW_HEIGHT)];
 		[animatorBGView setImage:[NSImage imageNamed:kMBMAnimationBackgroundImageName]];
+		[animatorBGView setImageScaling:NSScaleNone];
 		[[self view] addSubview:animatorBGView];
 		_animatorView = animatorBGView;
 		
@@ -49,7 +57,8 @@
 			NSTextField		*aField = [[[NSTextField alloc] initWithFrame:fieldFrame] autorelease];
 			[aField setStringValue:[itemDict valueForKey:kMBMConfirmationLocalizedTitleKey]];
 			[aField setAlignment:NSLeftTextAlignment];
-			[aField setTextColor:[NSColor blackColor]];
+			[aField setTextColor:TEXT_UNSELECTED_COLOR];
+			[aField setFont:[NSFont systemFontOfSize:16.0f]];
 			[aField setBackgroundColor:[NSColor clearColor]];
 			[aField setEditable:NO];
 			[aField setBezeled:NO];
@@ -76,8 +85,36 @@
 
 - (void)setSelectedStep:(NSInteger)aSelectedStep {
 	if (_selectedStep != aSelectedStep) {
+		[self animateMarkerFromItem:_selectedStep toItem:aSelectedStep];
 		_selectedStep = aSelectedStep;
 	}
+}
+
+- (void)animateMarkerFromItem:(NSInteger)fromItem toItem:(NSInteger)toItem {
+	
+	//	Try to get our respective views
+	NSTextField	*fromField = nil;
+	NSTextField	*toField = nil;
+	if ((fromItem != kMBMInvalidStep) && ([self.subviewList count] > (NSUInteger)fromItem)) {
+		fromField = [self.subviewList objectAtIndex:fromItem];
+	}
+	if ([self.subviewList count] > (NSUInteger)toItem) {
+		toField = [self.subviewList objectAtIndex:toItem];
+	}
+	
+	//	If we don't have at least the to view, there is nothing to do
+	if (toField == nil) {
+		return;
+	}
+	
+	//	Set the toField color properly
+	[toField setTextColor:TEXT_SELECTED_COLOR];
+	
+	//	Set the fromField color, if there is a field
+	if (fromField) {
+		[fromField setTextColor:TEXT_UNSELECTED_COLOR];
+	}
+	
 }
 
 @end
