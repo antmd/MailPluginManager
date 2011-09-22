9de322e3db3b38f052d989a1ae826e02c6d8f7c3
Scott Little <scott@littleknownsoftware.com>
2011-09-23 01:05:55 +0200

    Add progress view and installation summary view.

--- Mail Bundle Manager/Mail Bundle Manager/MBMInstallerController.h
+++ Mail Bundle Manager/Mail Bundle Manager/MBMInstallerController.h
@@ -12,12 +12,18 @@
 #import "MBMInstallationModel.h"
 #import "MBMAnimatedListController.h"
 
-@interface MBMInstallerController : NSWindowController {
+@interface MBMInstallerController : NSWindowController <NSTableViewDataSource> {
 	NSScrollView *_displayTextScrollView;
+	NSView *_displayProgressView;
+	NSProgressIndicator *_progressBar;
+	NSTextField *_displayProgressTextView;
+	NSTextField *_displayProgressLabel;
+	NSTableView *_installationSummaryTable;
+	NSScrollView *_displayInstallationPreview;
 }
 
 @property	(nonatomic, retain)	MBMInstallationModel		*installationModel;
-@property	(nonatomic, retain)	MBMAnimatedListController	*installListController;
+@property	(nonatomic, retain)	MBMAnimatedListController	*animatedListController;
 @property	(nonatomic, assign)	NSInteger					currentInstallStep;
 
 @property	(assign) IBOutlet NSImageView	*backgroundImageView;
@@ -27,8 +33,14 @@
 @property	(assign) IBOutlet NSButton		*previousStepButton;
 @property	(assign) IBOutlet NSButton		*actionButton;
 
-@property	(assign) IBOutlet NSText		*displayTextView;
-@property	(assign) IBOutlet NSScrollView	*displayTextScrollView;
+@property	(assign) IBOutlet NSText				*displayTextView;
+@property	(assign) IBOutlet NSScrollView			*displayTextScrollView;
+@property	(assign) IBOutlet NSView				*displayProgressView;
+@property	(assign) IBOutlet NSScrollView			*installationSummaryView;
+@property	(assign) IBOutlet NSProgressIndicator	*progressBar;
+@property	(assign) IBOutlet NSTextField			*displayProgressTextView;
+@property	(assign) IBOutlet NSTextField			*displayProgressLabel;
+@property	(assign) IBOutlet NSTableView			*installationSummaryTable;
 
 - (id)initWithInstallationModel:(MBMInstallationModel *)aModel;
 
--- Mail Bundle Manager/Mail Bundle Manager/MBMInstallerController.m
+++ Mail Bundle Manager/Mail Bundle Manager/MBMInstallerController.m
@@ -7,25 +7,34 @@
 //
 
 #import "MBMInstallerController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface MBMInstallerController ()
 - (void)updateCurrentConfigurationToStep:(NSInteger)toStep;
+- (void)startInstall;
+- (void)showContentView:(NSView *)aView;
 @end
 
 @implementation MBMInstallerController
 
 @synthesize installationModel = _installationModel;
-@synthesize installListController = _installListController;
+@synthesize animatedListController = _animatedListController;
 @synthesize currentInstallStep = _currentInstallStep;
 
 @synthesize backgroundImageView = _backgroundImageView;
 @synthesize installStepsView = _installStepsView;
 @synthesize titleTextField = _titleTextField;
 @synthesize displayWebView = _displayWebView;
+@synthesize displayProgressTextView = _displayProgressTextView;
+@synthesize displayProgressLabel = _displayProgressLabel;
+@synthesize installationSummaryTable = _installationSummaryTable;
+@synthesize installationSummaryView = _displayInstallationPreview;
 @synthesize previousStepButton = _previousStepButton;
 @synthesize actionButton = _actionButton;
 @synthesize displayTextView = _displayTextView;
 @synthesize displayTextScrollView = _displayTextScrollView;
+@synthesize displayProgressView = _displayProgressView;
+@synthesize progressBar = _progressBar;
 
 
 - (id)initWithInstallationModel:(MBMInstallationModel *)aModel {
@@ -52,7 +61,7 @@
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 	//	Create the install steps view
-	self.installListController = [[[MBMAnimatedListController alloc] initWithContentList:self.installationModel.confirmationStepList inView:self.installStepsView] autorelease];
+	self.animatedListController = [[[MBMAnimatedListController alloc] initWithContentList:self.installationModel.confirmationStepList inView:self.installStepsView] autorelease];
 	
 	//	Set the window title from the installation Model
 	NSString	*localizedFormat = NSLocalizedString([[self window] title], @"");
@@ -61,6 +70,18 @@
 	//	Get the image for the background from the installationModel
 	NSImage	*bgImage = [[[NSImage alloc] initWithContentsOfFile:self.installationModel.backgroundImagePath] autorelease];
 	[self.backgroundImageView setImage:bgImage];
+	
+	//	Set up the title label to be animatable
+	CATextLayer	*textLayer = [CATextLayer layer];
+	[self.titleTextField setLayer:textLayer];
+	[self.titleTextField setWantsLayer:YES];
+	textLayer.fontSize = 16.0;
+	CGColorRef	aColor = CGColorCreateGenericGray(0.000, 1.000);
+	textLayer.foregroundColor = aColor;
+	CGColorRelease(aColor);
+	 
+	 //	Localize the progress label
+	[self.displayProgressLabel setStringValue:NSLocalizedString(@"Please wait while I install your plugin…", @"Title description for progress view during installation")];
 	
 	//	Localize the Go Back step as well
 	[self.previousStepButton setTitle:NSLocalizedString(@"Go Back", @"Go Back button text for installation")];
@@ -75,7 +96,8 @@
 		
 		//	Validate that we don't go beyond our range
 		if ([self.installationModel.confirmationStepList count] <= (NSUInteger)aCurrentInstallStep) {
-			//	Don't do anything for the moment
+			//	Call our display the installation progress method and return
+			[self startInstall];
 			return;
 		}
 		
@@ -100,33 +122,102 @@
 	//	Get some values out
 	NSString	*type = [newStepDict valueForKey:kMBMConfirmationTypeKey];
 	NSString	*contentPath = [newStepDict valueForKey:kMBMPathKey];
+	BOOL		isConfirmed = [type isEqualToString:kMBMConfirmationTypeConfirm];
 	BOOL		isContentHTML = [[newStepDict valueForKey:kMBMPathIsHTMLKey] boolValue];
 	
 	//	Load the contents 
 	//	Is it html?
 	if (isContentHTML) {
-		[self.displayWebView setHidden:NO];
-		[self.displayTextScrollView setHidden:YES];
+		[self showContentView:self.displayWebView];
 		[self.displayWebView setMainFrameURL:contentPath];
 	}
+	else if (isConfirmed) {
+		//	Set the datasource for the installation summary tableview
+		[self.installationSummaryTable setDataSource:self];
+		[self showContentView:self.installationSummaryView];
+	}
 	else {
-		[self.displayTextScrollView setHidden:NO];
-		[self.displayWebView setHidden:YES];
+		[self showContentView:self.displayTextScrollView];
 		[self.displayTextView readRTFDFromFile:contentPath];
 	}
 	
-	//	Title above the webview
-	[self.titleTextField setStringValue:[newStepDict valueForKey:kMBMConfirmationLocalizedTitleKey]];
+	//	Title above the webview (its layer is used to animate it)
+	((CATextLayer *)self.titleTextField.layer).string = [newStepDict valueForKey:kMBMConfirmationLocalizedTitleKey];
 	
 	//	Configure the two buttons at the bottom
 	NSString	*actionTitle = NSLocalizedString(@"Continue", @"Continue button text for installation");
-	if ([type isEqualToString:kMBMConfirmationTypeConfirm]) {
+	if (isConfirmed) {
 		actionTitle = NSLocalizedString(@"Install", @"Install button text for installation");
 	}
 	[self.actionButton setTitle:actionTitle];
 	[self.previousStepButton setEnabled:(toStep != 0)];
 	
-	self.installListController.selectedStep = toStep;
+	self.animatedListController.selectedStep = toStep;
+}
+
+- (void)startInstall {
+	//	Show the progress view
+	[self showContentView:self.displayProgressView];
+	
+	//	Disable the buttons
+	[self.previousStepButton setEnabled:NO];
+	[self.actionButton setEnabled:NO];
+	
+	//	Set the total value for the progress bar
+	[self.progressBar setMaxValue:[self.installationModel.installationItemList count]+1];
+	
+	//	Set up some notification watches
+	//id notificationWatcher = 
+	[[NSNotificationCenter defaultCenter] addObserverForName:@"sjl" object:self.installationModel queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+		//	Update the UI
+		[self.displayProgressTextView setStringValue:[[note userInfo] valueForKey:@"install-description"]];
+		[self.progressBar setDoubleValue:[[[note userInfo] valueForKey:@"progress-value"] doubleValue]];
+	}];
+	
+	NSArray	*textList = [NSArray arrayWithObjects:@"A File.txt", @"My Big File.app", @"Bundle Manager.app", nil];
+
+	CGFloat	delayTime = 0.0f;
+	for (NSString *text in textList) {
+		NSDictionary	*myDict = [NSDictionary dictionaryWithObjectsAndKeys:text, @"install-description", 
+								   [NSNumber numberWithDouble:delayTime + 1.0f], @"progress-value",
+								   nil];
+		NSNotification	*dumNote = [NSNotification notificationWithName:@"sjl" object:self.installationModel userInfo:myDict];
+		[[NSNotificationCenter defaultCenter] performSelector:@selector(postNotification:) withObject:dumNote afterDelay:delayTime];
+		delayTime = delayTime + 1.0f;
+	}
+	
+	
+}
+
+- (void)showContentView:(NSView *)aView {
+	//	Handle the transition with a small animation
+	[NSAnimationContext beginGrouping];
+	[[NSAnimationContext currentContext] setDuration:0.25f];
+	//	After the animation, HIDE the view because otherwise they eat the events (thanks CALayer!)
+	[[NSAnimationContext currentContext] setCompletionHandler:^(void) {
+		[self.displayWebView setHidden:(aView != self.displayWebView)];
+		[self.displayTextScrollView setHidden:(aView != self.displayTextScrollView)];
+		[self.displayProgressView setHidden:(aView != self.displayProgressView)];
+		[self.installationSummaryView setHidden:(aView != self.installationSummaryView)];
+	}];
+	[[self.displayWebView animator] setAlphaValue:(aView == self.displayWebView)?1.0f:0.0f];
+	[[self.displayTextScrollView animator] setAlphaValue:(aView == self.displayTextScrollView)?1.0f:0.0f];
+	[[self.displayProgressView animator] setAlphaValue:(aView == self.displayProgressView)?1.0f:0.0f];
+	[[self.installationSummaryView animator] setAlphaValue:(aView == self.installationSummaryView)?1.0f:0.0f];
+	[NSAnimationContext endGrouping];
+}
+
+
+#pragma mark - TableView DataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+	return 1;
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+	NSMutableAttributedString	*attrString = [[[NSMutableAttributedString alloc] initWithString:@"File To Install\nSome Description"] autorelease];
+	[attrString setAttributes:[NSDictionary dictionaryWithObject:[NSColor redColor] forKey:NSForegroundColorAttributeName] range:NSMakeRange(0, 16)];
+	return ([[tableColumn identifier] isEqualToString:@"icon"]?[NSImage imageNamed:@"InstallDocument"]:attrString);
 }
 
 @end
